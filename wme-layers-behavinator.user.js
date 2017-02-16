ec55f2c7d85bb8ecd44538272e6d13f5c55f2654
Glodenox <github@tomputtemans.com>
2017-02-16 22:52:04 +0100

    First version of userscript

---
+++ wme-layers-behavinator.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        WME Beta layers menu behavinator
+// @namespace   http://www.tomputtemans.com/
+// @description Makes the layers menu appear when hovering over the button and stick when clicked
+// @include     https://beta.waze.com/*editor/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+(function() {
+  function init(e) {
+    if (e && e.user == null) {
+      return;
+    }
+
+    var switcher = document.querySelector('.layer-switcher');
+    if (!switcher) {
+      setTimeout(init, 300);
+    }
+    var content = switcher.querySelector('.content');
+    switcher.addEventListener('click', function(e) {
+      console.log('click received', e);
+      if (e.target.classList.contains('toolbar-button')) {
+        console.log('click intercepted');
+        e.preventDefault();
+        content.classList.toggle('forced');
+        content.classList.toggle('not-visible', !content.classList.contains('forced'));
+        document.getElementById('editor-container').style.paddingRight = (content.classList.contains('forced') ? '230px' : '0');
+      }
+    });
+    switcher.addEventListener('mouseover', function() {
+      if (!content.classList.contains('forced')) {
+        content.classList.remove('not-visible');
+      }
+    });
+    switcher.addEventListener('mouseleave', function() {
+      if (!content.classList.contains('forced')) {
+        content.classList.add('not-visible');
+      }
+    });
+  }
+
+  init();
+})();
